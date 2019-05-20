777b9bc659517b92a8b9b52e33091aec28f10a8c
vkindrat <49250027+vkindrat@users.noreply.github.com>
2019-05-20 15:46:35 -0300

    Update placar.js

--- js/placar.js
+++ js/placar.js
@@ -1,3 +1,9 @@
+
+$("#sync").click(sincronizaPlacar);
+
+
+
+
 function inserePlacar(){
 
 	
@@ -71,3 +77,57 @@ function scrollPlacar(){
 
 	}, 1000);
 }
+
+
+function sincronizaPlacar(){
+
+	var placar = [];
+	var linhas = $("tbody>tr");
+
+	linhas.each(function(){
+
+		var usuario = $(this).find("td:nth-child(1)").text();
+		var palavras = $(this).find("td:nth-child(2)").text();
+
+		var score = {
+			usuario: usuario,
+			pontos: palavras
+		};
+
+		placar.push(score);
+	});
+
+
+
+
+
+	var dados = {
+
+	placar: placar
+};
+
+
+
+	$.post("http://localhost:3000/placar", dados, function(){
+
+		console.log("Sincronizado com sucesso.");
+});
+
+}
+
+
+function atualizaPlacar(){
+
+
+	 $.get("http://localhost:3000/placar",function(data){
+        $(data).each(function(){
+            var linha = novaLinha(this.usuario, this.pontos);
+
+            linha.find(".botao-remover").click(removeLinha);
+
+            $("tbody").append(linha);
+});
+
+    });
+
+	}
