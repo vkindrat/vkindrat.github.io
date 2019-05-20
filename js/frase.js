eb769f35b45b98a7045a05d1c9a94648a95bac7e
vkindrat <49250027+vkindrat@users.noreply.github.com>
2019-05-20 15:44:16 -0300

    Update frase.js

--- js/frase.js
+++ js/frase.js
@@ -1,10 +1,28 @@
 
 $("#botao-frase").click(FraseAleatoria);
+$("#botao-id").click(buscaFrase);
 
 
 function FraseAleatoria(){
 
-	$.get("http://localhost:3000/frases", trocaFraseAleatoria);
+
+
+	$("#spinner").toggle();
+	$.get("http://localhost:3000/frases", trocaFraseAleatoria)
+	.fail(function(){
+
+		$("#msg-erro").toggle();
+		setTimeout(function(){
+			$("#msg-erro").toggle();
+	},2500);
+		})
+		.always(function(){
+
+			$("#spinner").toggle();
+
+		});
+
+	
 
 }
 
@@ -14,4 +32,43 @@ function trocaFraseAleatoria(data){
 	var numeroAleatorio = Math.floor(Math.random() * data.length);
 
 	frase.text(data[numeroAleatorio].texto);
+	atualizaTamanhoFrase();
+	atualizaTempoInicial(data[numeroAleatorio].tempo);
+}
+
+
+function buscaFrase(){
+
+	$("#spinner").toggle();
+
+	var fraseId = $("#frase-id").val();
+	var dados = {id: fraseId};
+
+
+	$.get("http://localhost:3000/frases", dados, trocaFrase)
+	.fail(function(){
+
+		$("#msg-erro").toggle();
+		setTimeout(function(){
+			$("#msg-erro").toggle();
+	},2500);
+		})
+		.always(function(){
+
+			$("#spinner").toggle();
+
+		});
+
+}
+
+
+function trocaFrase(data){
+
+	console.log(data);
+
+	var frase = $(".frase");
+	frase.text(data.texto);
+	atualizaTamanhoFrase();
+	atualizaTempoInicial(data.tempo);
+
 }
