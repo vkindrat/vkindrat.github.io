d7ee0b517bfb95e73155ada2d92b59fe816c1d13
vkindrat <49250027+vkindrat@users.noreply.github.com>
2019-06-12 14:38:53 -0300

    Add files via upload

---
+++ js/portfolio.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+	var header = document.getElementsByClassName("cabecalho");
+	var background = document.getElementsByClassName("background");
+	var troca = document.getElementsByClassName("logo");
+
+	troca[0].onclick = function(){
+    background[0].style.backgroundColor = "hue-rotate(30deg)";
+    header[0].style.backgroundColor = "hue-rotate(30deg)";
+
+   console.log(background.filter.value);
+
+		
+		
+	};
+
+
+	
